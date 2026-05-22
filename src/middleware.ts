93f83cc3db12de076a6cc37f8114ada46e7eced6
PeraltaHv <peralta29v@gmail.com>
2026-05-22 13:37:27 -0300

    se emparejo las lineas del middlewre

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,16 @@
 import { withAuth } from "next-auth/middleware";
 
 export default withAuth({
-  pages: {
-    signIn: "/login",
-  },
+pages: {
+signIn: "/login",
+},
 });
 
-// Especificamos ÚNICAMENTE las pantallas de la app que requieren sesión
 export const config = {
-  matcher: [
-    "/landing",
-    "/landing/:path*",
-    "/admin",
-    "/admin/:path*"
-  ],
+matcher: [
+"/landing",
+"/landing/:path*",
+"/admin",
+"/admin/:path*",
+],
 };
